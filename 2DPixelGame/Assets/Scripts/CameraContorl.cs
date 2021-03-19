96de5a9989bc5f23b64c92812680fcd5910644c8
0608434 <71476500+0608434@users.noreply.github.com>
2021-03-19 12:05:32 +0800

    攝影機

---
+++ 2DPixelGame/Assets/Scripts/CameraContorl.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraContorl: MonoBehaviour
+{
+    [Header("追蹤速度"), Range(0, 50)]
+    public float speed = 1.5f;
+    [Header("上下邊界")]
+    public Vector2 limitY = new Vector2(-5, 5);
+    [Header("左右邊界")]
+    public Vector2 limitX = new Vector2(-5, 5);
+
+
+}
