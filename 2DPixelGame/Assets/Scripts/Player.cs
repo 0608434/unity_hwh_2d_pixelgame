ef89f76f55049d5179046db2378ae15e1e8a8383
0608434 <71476500+0608434@users.noreply.github.com>
2021-04-09 09:44:36 +0800

    道具機率

--- 2DPixelGame/Assets/Scripts/Player.cs
+++ 2DPixelGame/Assets/Scripts/Player.cs
@@ -61,12 +61,12 @@ public class Player : MonoBehaviour
 
     private void Move() 
     {
-        print("移動");
+        //print("移動");
         float h = joystick.Horizontal;
-        print("水平:" + h);
+        //print("水平:" + h);
 
         float v = joystick.Vertical;
-        print("垂直:" + v);
+        //print("垂直:" + v);
 
         //變形元件.位移(水平*速度*一幀的時間，垂直*速度*一幀的時間，0)
         tra.Translate(h * speed * Time.deltaTime, v * speed * Time.deltaTime, 0);
@@ -85,8 +85,8 @@ public class Player : MonoBehaviour
         //2D 物理 圓形碰撞 (中心點，半徑，方向，距離，圖層)
         RaycastHit2D hit = Physics2D.CircleCast(transform.position, rangeAttack, transform.up,0, 1 << 8);
 
-        //如果 碰到物件存在 並且 碰到的物件 標籤 為 道具 就刪除(碰到的碰撞器的遊戲物件)
-        if (hit && hit.collider.tag == "道具") Destroy(hit.collider.gameObject);
+        //如果 碰到物件存在 並且 碰到的物件 標籤 為 取得道具腳本並呼叫道具掉落方法
+        if (hit && hit.collider.tag == "道具") hit.collider.GetComponent<Item>().DropProp();
 
     }
 
